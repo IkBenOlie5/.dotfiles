09d69b03033276559b1e9036b0c0f505addad45c
IkBenOlie5 <olivier@lombok.nl>
2021-10-10 08:19:04 +0200

    update

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -5,8 +5,9 @@ alias q="exit"
 alias sz="source ~/.zshrc"
 alias va="source ./.venv/bin/activate"
 alias vd="deactivate"
-alias py="python3.9"
+alias py="python3.10"
 alias cat="bat"
+alias mkd="mkdir"
 
 #brew services
 alias start="brew services start"
